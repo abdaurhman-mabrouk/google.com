f99bc1771ca929bb838d674250d68a29b731e25d
abdaurhman-mabrouk <aabdo6629@gmail.com>
2024-07-14 21:28:40 +0300

    Prepairing NPM and Install axios Library

---
+++ main.js
@@ -0,0 +1,11 @@
+const axios = require('node_modules/axios/dist/axios.js');
+
+// Constants
+const productsLink = document.getElementById('productsLink');
+const accountLink = document.getElementById('accountLink');
+
+productsLink.onclick = () => {
+  alert('Working');
+};
+
+axios;
