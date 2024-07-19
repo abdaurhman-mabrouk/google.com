5ff0713d6fbf5aefc5035f2414424c97242b0877
abdaurhman-mabrouk <aabdo6629@gmail.com>
2024-07-20 01:42:07 +0300

    Edit Main.js File

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const axios = require('node_modules/axios/dist/axios.js');
-
 // Constants
 const productsLink = document.getElementById('productsLink');
 const accountLink = document.getElementById('accountLink');
@@ -7,5 +5,3 @@ const accountLink = document.getElementById('accountLink');
 productsLink.onclick = () => {
   alert('Working');
 };
-
-axios;
